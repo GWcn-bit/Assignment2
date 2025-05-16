6345ebeb5344d4e9cf4cae53cf381afcaf63e0a0
16478 <16478@10.127.34.227>
2025-05-16 11:13:53 -0400

    noing

---
+++ src/Quiz/Tips.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Quiz;
+
+/**
+ *
+ * @author 16478
+ */
+public class Tips {
+    
+}
