14978c3a9050a9ab3fa647e4a405989882ac8f8c
16478 <16478@10.127.34.227>
2025-05-20 10:52:23 -0400

    noing

--- src/Quiz/Tips.java
+++ src/Quiz/Tips.java
@@ -9,5 +9,28 @@ package Quiz;
  * @author 16478
  */
 public class Tips {
-    
+    public void showTips() {
+        System.out.println("Try to use screens in a balanced way.");       
+    }   
+}
+
+class LowTips extends Tips{
+    @Override
+    public void showTips() {
+        System.out.println("You're doing great! Keep healthy habits!");
+    }
+}
+
+class MediumTips extends Tips {
+    @Override
+    public void showTips() {
+        System.out.println("Try taking 10-minute breaks every hour.");
+    }   
+}
+
+class HighTips extends Tips{
+    @Override
+    public void showTips() {
+        System.out.println("Please talk to someone or reduce screen time ASAP!");
+    }
 }
