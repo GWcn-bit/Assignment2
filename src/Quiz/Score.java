34c3fb157e299f5f10c3a27b09b422b3f33071ae
16478 <16478@10.127.34.227>
2025-05-20 10:51:51 -0400

    noing

--- src/Quiz/Score.java
+++ src/Quiz/Score.java
@@ -9,5 +9,26 @@ package Quiz;
  * @author 16478
  */
 public class Score {
+    private int points = 0;
     
+    public Score(int startPoints){
+       points = startPoints; 
+    }
+    
+    public void addPoints(int p){
+      points += p;  
+    }
+    public int getPoints() {
+        return points;
+    }
+    
+    public String getRiskLevel(){
+        if(points<=40){
+            return"Low";
+        }else if(points <= 69){
+            return "Medium";            
+        }else{
+            return "High";
+        }        
+    }    
 }
